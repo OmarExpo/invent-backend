d11d403197652f234834eb183def0a8205b911f3
unknown <omar1807@stud.kea.dk>
2022-12-15 15:58:24 +0100

    updated serverjs

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://invent-app-xi.vercel.app"],
-    // credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "https://invent-app-xi.vercel.app"],
+//     // credentials: true,
+//   })
+// );
+app.use(cors("*"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
