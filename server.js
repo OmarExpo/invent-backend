1e09a1c87e14b945ca6cca5e4e5e8c9d1bff1a16
unknown <omar1807@stud.kea.dk>
2022-12-29 16:19:31 +0300

    server.js updates

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
@@ -24,33 +29,28 @@ app.use(
     credentials: true,
   })
 );
-// app.use(
-//   cors({
-//     origin: "*",
-//     "Access-Control-Allow-Headers": "Authorization",
-//     credentials: true,
-//   })
-// );
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
-// Main Route
+// Routes
 app.get("/", (req, res) => {
-  res.send("Welcome to Home Page");
+  res.send("Home Page");
 });
 
-// error middleware
+// Error Middleware
 app.use(errorHandler);
-
-// Connect to MongoDB and start server
+// Connect to DB and start server
 const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
   })
   .catch((err) => console.log(err));
