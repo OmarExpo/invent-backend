ec050f25feb8753233cf0a6af747ad069402fa2a
unknown <omar1807@stud.kea.dk>
2022-12-29 16:35:30 +0300

    server.js updates

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const router = express.Router();
+const protect = require("../middleWare/authMiddleware");
 const {
   createProduct,
   getProducts,
@@ -6,16 +8,12 @@ const {
   deleteProduct,
   updateProduct,
 } = require("../controllers/productController");
-const router = express.Router();
 const { upload } = require("../utils/fileUpload");
 
-router.post("/", upload.single("image"), createProduct);
-
-router.get("/", getProducts);
-router.get("/:id", getProduct);
-
-router.delete("/:id", deleteProduct);
-
-router.put("/:id", upload.single("image"), updateProduct);
+router.post("/", protect, upload.single("image"), createProduct);
+router.patch("/:id", protect, upload.single("image"), updateProduct);
+router.get("/", protect, getProducts);
+router.get("/:id", protect, getProduct);
+router.delete("/:id", protect, deleteProduct);
 
 module.exports = router;
