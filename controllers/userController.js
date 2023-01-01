998761a0ff3064aac230dad13b2a0050bde657e4
unknown <omar1807@stud.kea.dk>
2023-01-01 15:05:27 +0100

    updates some files

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,7 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
     path: "/",
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
-    sameSite: "strict",
+    sameSite: "Strict",
     secure: true,
   });
 
@@ -98,7 +98,7 @@ const loginUser = asyncHandler(async (req, res) => {
     path: "/",
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
-    sameSite: "strict",
+    sameSite: "Strict",
     secure: true,
   });
 
@@ -125,7 +125,7 @@ const logout = asyncHandler(async (req, res) => {
     path: "/",
     httpOnly: true,
     expires: new Date(0),
-    sameSite: "strict",
+    sameSite: "Strict",
     secure: true,
   });
   return res.status(200).json({ message: "Successfully Logged Out" });
