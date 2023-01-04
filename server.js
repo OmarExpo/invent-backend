13edc00dc428c59907c2831c50253670e1a15bbe
unknown <omar1807@stud.kea.dk>
2023-01-04 21:47:10 +0100

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
