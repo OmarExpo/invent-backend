8c050ea32bbf7d9f2d5b4b075e782a23c0865ee9
unknown <omar1807@stud.kea.dk>
2023-01-08 21:45:41 +0100

    updates some files

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -13,9 +13,9 @@ const { upload } = require("../utils/fileUpload");
 router.post("/", protect, upload.single("image"), createProduct);
 // router.post("/", upload.single("image"), createProduct);
 router.patch("/:id", protect, upload.single("image"), updateProduct);
-router.get("/", protect, getProducts);
+// router.get("/", protect, getProducts);
 router.get("/", getProducts);
-// router.get("/:id", protect, getProduct);
+router.get("/:id", protect, getProduct);
 router.delete("/:id", protect, deleteProduct);
 
 module.exports = router;
