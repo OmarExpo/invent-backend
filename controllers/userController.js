abfa1c320cf1663f3acfb902831fbc5443809400
unknown <omar1807@stud.kea.dk>
2023-01-09 16:59:09 +0100

    user controller route

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,7 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // Send the HTTP-only cookie
   res.cookie("token", token, {
-    path: "/",
+    // path: "/",
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
     sameSite: "None",
@@ -96,7 +96,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   // Send the HTTP-only cookie
   res.cookie("token", token, {
-    path: "/",
+    // path: "/",
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
     sameSite: "None",
